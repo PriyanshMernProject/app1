e971e3bb160512d0bb9fbf81b24447c6a031cb24
Priyansh <50priyansh.kumar@gmail.com>
2024-11-15 16:39:50 +0530

    add server IP

--- client/src/Pages/Login/index.jsx
+++ client/src/Pages/Login/index.jsx
@@ -16,7 +16,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await fetch("http://localhost:3001/api/v1/user/login", {
+      const res = await fetch("http://3.110.153.253/:3001/api/v1/user/login", {
         method: "POST",
         body: JSON.stringify(data),
         headers: {
